cce4034691efa76180c393d6871b456dcd753d3f
mplowey28 <matthew.plowey@gmail.com>
2020-11-14 20:36:38 +0100

    final version

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,13 +1,13 @@
 import express, { Express } from "express";
 import BodyParser from "body-parser";
 import mongoose from "mongoose";
+import path from "path";
 import cors from "cors";
 import http from "http";
 import ioserver, { Socket } from "socket.io";
 import todoRoutes from "./routes";
 import TodoList from "./models/todoList";
 import * as dotenv from "dotenv";
-import helmet from "helmet";
 dotenv.config();
 
 const PORT: string | number = process.env.PORT || 4000;
@@ -15,7 +15,6 @@ const app: Express = express();
 const server = http.createServer(app);
 const io = ioserver(server);
 
-app.use(helmet());
 app.use(cors());
 app.use(BodyParser.json());
 app.use(todoRoutes);
@@ -40,17 +39,37 @@ const DBPASSWORD = process.env.MONGO_PASSWORD;
 const DBNAME = process.env.MONGO_DB;
 
 const uri: string = `mongodb+srv://${DBUSER}:${DBPASSWORD}@cluster0.cdfh2.mongodb.net/${DBNAME}?retryWrites=true&w=majority`;
-const options = { useNewUrlParser: true, useUnifiedTopology: true };
+const options = {
+	useNewUrlParser: true,
+	useUnifiedTopology: true,
+	useCreateIndex: true,
+};
 mongoose.set("useFindAndModify", false);
 
-mongoose.connect(uri, options).catch(error => {
-	throw error;
-});
+mongoose
+	.connect(uri, options)
+	.then(() => console.log("MongoDB Connected..."))
+	.catch(error => {
+		throw error;
+	});
 
 app.use((req, res) => {
 	res.end("Page Not Found");
 });
 
+if (process.env.NODE_ENV === "production") {
+	// Set static folder
+	app.use(express.static("client/build"));
+
+	app.get("*", (req, res) => {
+		res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+	});
+}
+
 server.listen(process.env.PORT || PORT, () => {
+	console.log(`App Server Listening at ${PORT}`);
+});
+
+app.listen(process.env.PORT || PORT, () => {
 	console.log(`App Server Listening at ${PORT}`);
 });
